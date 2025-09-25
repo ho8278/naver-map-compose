fa24ec7db82852f761acdb9c619c406145b4b4db
ho8278 <ho8278@naver.com>
2025-09-25 16:38:47 +0900

    Modify SimpleCluster Demo to use Clustering

--- app/src/main/java/com/naver/maps/map/compose/demo/clustering/ClusteringScreen.kt
+++ app/src/main/java/com/naver/maps/map/compose/demo/clustering/ClusteringScreen.kt
@@ -15,33 +15,42 @@
  */
 package com.naver.maps.map.compose.demo.clustering
 
+import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraPosition
-import com.naver.maps.map.clustering.Clusterer
-import com.naver.maps.map.clustering.DefaultLeafMarkerUpdater
-import com.naver.maps.map.clustering.LeafMarkerInfo
-import com.naver.maps.map.compose.DisposableMapEffect
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.compose.Clustering
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
 import com.naver.maps.map.compose.NaverMap
 import com.naver.maps.map.compose.demo.R
 import com.naver.maps.map.compose.demo.common.DefaultTopAppBar
 import com.naver.maps.map.compose.rememberCameraPositionState
 import com.naver.maps.map.overlay.Marker
-import com.naver.maps.map.overlay.Overlay
 import com.naver.maps.map.util.MapConstants
 import com.naver.maps.map.util.MarkerIcons
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalNaverMapApi::class)
 @Composable
 fun ClusteringScreen(upPress: () -> Unit) {
     Scaffold(
@@ -64,27 +73,14 @@ private fun Clustering() {
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition(MapConstants.EXTENT_KOREA.center, 4.0)
     }
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
     NaverMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState,
     ) {
-        var clusterManager by remember { mutableStateOf<Clusterer<ItemKey>?>(null) }
-        DisposableMapEffect(Unit) { map ->
-            if (clusterManager == null) {
-                clusterManager = Clusterer.Builder<ItemKey>()
-                    .leafMarkerUpdater(object : DefaultLeafMarkerUpdater() {
-                        override fun updateLeafMarker(info: LeafMarkerInfo, marker: Marker) {
-                            super.updateLeafMarker(info, marker)
-                            marker.icon = ICONS[info.tag as Int]
-                            marker.onClickListener = Overlay.OnClickListener {
-                                clusterManager?.remove(info.key as ItemKey)
-                                true
-                            }
-                        }
-                    })
-                    .build()
-                    .apply { this.map = map }
-            }
+        var clusterItems by remember { mutableStateOf(emptyMap<ItemKey, Int>()) }
+        LaunchedEffect(Unit) {
             val keyTagMap = buildMap(5000) {
                 val south = MapConstants.EXTENT_KOREA.southLatitude
                 val west = MapConstants.EXTENT_KOREA.westLongitude
@@ -101,12 +97,49 @@ private fun Clustering() {
                     )
                 }
             }
-            clusterManager?.addAll(keyTagMap)
-            onDispose {
-                clusterManager?.clear()
-            }
+            clusterItems = keyTagMap
         }
+
+        Clustering(
+            items = clusterItems,
+            clusterContent = {
+                Column(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .background(Color.Yellow, RoundedCornerShape(8.dp))
+                ) {
+                    Text("Cluster(${it.size})")
+                }
+            },
+            leafContent = {
+                Column(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .background(Color.Cyan, RoundedCornerShape(8.dp))
+                ) {
+                    Text("Leaf(${it.tag})")
+                }
+            },
+            onClickCluster = { info, _ ->
+                scope.launch {
+                    cameraPositionState.animate(
+                        CameraUpdate.toCameraPosition(
+                            CameraPosition(
+                                info.position,
+                                cameraPositionState.position.zoom + 2.0
+                            )
+                        )
+                    )
+                }
+                true
+            },
+            onClickLeaf = { info, _ ->
+                Toast.makeText(context, "Clicked! ${info.tag}", Toast.LENGTH_SHORT).show()
+                true
+            }
+        )
     }
 }
 
-private val ICONS = arrayOf(Marker.DEFAULT_ICON, MarkerIcons.BLUE, MarkerIcons.RED, MarkerIcons.YELLOW)
+private val ICONS =
+    arrayOf(Marker.DEFAULT_ICON, MarkerIcons.BLUE, MarkerIcons.RED, MarkerIcons.YELLOW)
